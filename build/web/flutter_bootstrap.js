6379dcbd2268930194c7205d39561fb2399779d3
Aflah Shadil k <shadilpml@gmail.com>
2026-02-02 12:43:06 +0530

    add_build

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cb4b5fff73850b2e42bd4de7cb9a4310a78ac4
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4155208804"
+    serviceWorkerVersion: "202765227"
   }
 });
